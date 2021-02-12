346cc712a4c0f659b90bb1f6843b282c692567cf
Christopher Begley <christopherbegley@mac.com>
2021-02-12 09:26:58 -0500

    change root to docs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 let server = app.listen(3000, function () {console.log('server listening on port 3000');}); 
 
